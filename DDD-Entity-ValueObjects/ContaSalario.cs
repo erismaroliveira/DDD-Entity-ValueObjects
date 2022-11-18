b0a8b571d1651cabba573afa90aecce74f2badb6
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-11-18 11:04:37 -0300

    Initial commit

---
+++ DDD-Entity-ValueObjects/ContaSalario.cs
@@ -0,0 +1,27 @@
+﻿namespace DDD_Entity_ValueObjects;
+
+public sealed class ContaSalario : Entity
+{
+    public Dinheiro Saldo { get; private set; } // --> Value Object
+
+    public ContaSalario(Dinheiro valor)
+    {
+        if (valor.Valor >= 0)
+            Saldo = valor;
+        else
+            throw new Exception("Saldo da conta negativo");
+    }
+
+    public void Deposito(Dinheiro valor)
+    {
+        Saldo += valor;
+    }
+    
+    public void Saque(Dinheiro valor)
+    {
+        if (Saldo.Valor >= valor.Valor)
+            Saldo -= valor;
+        else
+            throw new Exception("Saldo insuficiente");
+    }
+}
---
+++ DDD-Entity-ValueObjects/Dinheiro.cs
@@ -0,0 +1,41 @@
+﻿namespace DDD_Entity_ValueObjects;
+
+public sealed class Dinheiro : ValueObject<Dinheiro>
+{
+    public decimal Valor { get; private set; }
+
+	public Dinheiro(decimal valor)
+	{
+		Valor = valor;
+	}
+
+    public static Dinheiro operator +(Dinheiro valor1, Dinheiro valor2)
+	{
+		Dinheiro soma = new Dinheiro(valor1.Valor + valor2.Valor);
+		return soma;
+	}
+
+    public static Dinheiro operator -(Dinheiro valor1, Dinheiro valor2)
+	{
+		if (valor1.Valor < valor2.Valor)
+		{
+			throw new InvalidOperationException("Operação inválida");
+		}
+		else
+		{
+			Dinheiro diferenca = new Dinheiro(valor1.Valor - valor2.Valor);
+			return diferenca;
+		}
+	}
+
+	protected override bool EqualsCore(Dinheiro other)
+	{
+		return Valor == other.Valor;
+	}
+
+	protected override decimal GetHashCodeCore()
+	{
+		decimal hashCode = Valor.GetHashCode();
+		return hashCode;
+	}
+}
